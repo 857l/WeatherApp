771f322a09ccfca2a01279da7e81196cf3abee69
857l <nik.gusev.2017@bk.ru>
2025-12-10 12:21:20 +0300

    update WeatherCloud

--- app/src/main/java/ru/n857l/weatherapp/weather/data/WeatherCloud.kt
+++ app/src/main/java/ru/n857l/weatherapp/weather/data/WeatherCloud.kt
@@ -3,9 +3,52 @@ package ru.n857l.weatherapp.weather.data
 import com.google.gson.annotations.SerializedName
 
 data class WeatherCloud(
-    @SerializedName("main") val main: MainCloud
+    @SerializedName("main")
+    val main: Main,
+    @SerializedName("visibility")
+    val visibility: Int,
+    @SerializedName("wind")
+    val wind: Wind,
+    @SerializedName("clouds")
+    val clouds: Clouds,
+    @SerializedName("dt")
+    val dateTime: Long,
+    @SerializedName("sys")
+    val sun: Sun
 )
 
-data class MainCloud(
-    @SerializedName("temp") val temperature: Float
+data class Main(
+    @SerializedName("temp")
+    val temperature: Float,
+    @SerializedName("feels_like")
+    val feelsTemperature: Float,
+    @SerializedName("pressure")
+    val pressure: Int,
+    @SerializedName("humidity")
+    val humidity: Int,
+    @SerializedName("sea_level")
+    val seaLevelPressure: Int,
+    @SerializedName("grnd_level")
+    val groundLevelPressure: Int
+)
+
+data class Wind(
+    @SerializedName("speed")
+    val speed: Float,
+    @SerializedName("deg")
+    val degree: Int,
+    @SerializedName("gust")
+    val gust: Float
+)
+
+data class Clouds(
+    @SerializedName("all")
+    val all: Int
+)
+
+data class Sun(
+    @SerializedName("sunrise")
+    val sunrise: Long,
+    @SerializedName("sunset")
+    val sunset: Long
 )
